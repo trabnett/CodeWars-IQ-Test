c20e1c570a0c2d8650173542f84f4775234dd96f
Tim Rabnett <timrabnett@gmail.com>
2019-03-15 10:06:06 -0400

    codewars iq test. overcomplicated working solution

--- iq.js
+++ iq.js
@@ -1,15 +1,29 @@
-let num = "2 2 8 7 10 70"
+let num = "1 2 4 2 10 2"
 
 function iqTest(numbers){
-    nums = numbers.split(" ")
-    resArray = []
-  nums.forEach(function(i,idx) {
-      if (i % 2 === 0) {
+    let nums = numbers.split(" ")
+    let resArray = []
+    let res = 0
+    nums.forEach(function(i) {
+        if (i % 2 === 0) {
         resArray.push(2)
-      } else {
+        } else {
         resArray.push(1)
-      }
-})
-console.log(resArray)
+        }
+    })
+    resArray.forEach(function(j, idx){
+        if (j === resArray[0]) {
+        } else {
+            res = (idx + 1)
+        }
+    })
+    if (resArray[resArray.length - 1] === resArray[0]) {
+        return res
+    } else if (resArray[0] === resArray[1]){
+        return resArray.length
+    } else {
+        return 1
+    }
+
 }
 console.log(iqTest(num))
